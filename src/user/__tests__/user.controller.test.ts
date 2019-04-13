488d1690cad157b9a44bc4f5daf41be9bd998455
Kento Watanabe <k3nt0.watanabe@gmail.com>
2019-04-13 21:19:08 +0900

    fix(user): controller test (#4)
    fix(user): controller test

--- src/user/__tests__/user.controller.test.ts
+++ src/user/__tests__/user.controller.test.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserController } from '../user.controller'
+import { UserService } from '../user.service'
+import { User } from '../interfaces/user.interface'
 
 describe('User Controller', () => {
-  let controller: UserController
+  let userController: UserController
+  let userService: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService],
     }).compile()
 
-    controller = module.get<UserController>(UserController)
+    userController = module.get<UserController>(UserController)
+    userService = module.get<UserService>(UserService)
   })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined()
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const result: User[] = [
+        {
+          name: 'Pixel',
+          age: 2,
+        },
+      ]
+      jest.spyOn(userService, 'findAll').mockImplementation(() => result)
+
+      expect(await userController.findAll()).toBe(result)
+    })
   })
 })
