f17b2c87189bcbe0460528c51a31122fa512ba50
k3nt0w <k3nt0.watanabe@gmail.com>
2019-04-19 18:21:16 +0900

    fix: use constants for httpcode

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, HttpCode } from '@nestjs/common'
 import { UserService } from './user.service'
 import { User } from './interfaces/user.interface'
+import { statusCode } from '../http/statusCode'
 
 @Controller('user')
 export class UserController {
   public constructor(private readonly userService: UserService) {}
 
   @Get()
-  @HttpCode(201)
+  @HttpCode(statusCode.OK)
   public async findAll(): Promise<User[]> {
     return this.userService.findAll()
   }
